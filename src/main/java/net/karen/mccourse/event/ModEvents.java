191b7e1663acd74a587cfbe9b32f8938c9e947c3
karen-matsunaga <karen.f.matsunaga@gmail.com>
2024-10-10 17:37:39 -0300

    2024-10-10 Understanding Events and adding an custom event

--- src/main/java/net/karen/mccourse/event/ModEvents.java
+++ src/main/java/net/karen/mccourse/event/ModEvents.java
@@ -3,12 +3,17 @@ package net.karen.mccourse.event;
 import net.karen.mccourse.MCCourseMod;
 import net.karen.mccourse.command.ReturnHomeCommand;
 import net.karen.mccourse.command.SetHomeCommand;
+import net.karen.mccourse.item.ModItems;
 import net.karen.mccourse.item.custom.HammerItem;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,7 +23,7 @@ import net.minecraftforge.server.command.ConfigCommand;
 import java.util.HashSet;
 import java.util.Set;
 
-@Mod.EventBusSubscriber(modid = MCCourseMod.MOD_ID)
+@Mod.EventBusSubscriber(modid = MCCourseMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ModEvents {
 
     // Hammer's tool EVENT
@@ -68,6 +73,22 @@ public class ModEvents {
     public static void onPlayerCloned(PlayerEvent.Clone event) {
         event.getEntity().getPersistentData().putIntArray("mccourse.homepos",
                 event.getOriginal().getPersistentData().getIntArray("mccourse.homepos"));
+    }
+
+    // CUSTOM EVENT - An event example that to show if player hit on sheep entity using specific items
+    @SubscribeEvent
+    public static void livingDamage(LivingDamageEvent event) {
+        if(event.getEntity() instanceof Sheep) {
+            if(event.getSource().getDirectEntity() instanceof Player player) {
+                if(player.getItemInHand(InteractionHand.MAIN_HAND).getItem() == ModItems.ALEXANDRITE_AXE.get()) {
+                    MCCourseMod.LOGGER.info("Sheep was hit with Alexandrite Axe by " + player.getName().getString());
+                } else if(player.getItemInHand(InteractionHand.MAIN_HAND).getItem() == Items.DIAMOND) {
+                    MCCourseMod.LOGGER.info("Sheep was hit with DIAMOND by " + player.getName().getString());
+                } else {
+                    MCCourseMod.LOGGER.info("Sheep was hit with something else by " + player.getName().getString());
+                }
+            }
+        }
     }
 
 }
