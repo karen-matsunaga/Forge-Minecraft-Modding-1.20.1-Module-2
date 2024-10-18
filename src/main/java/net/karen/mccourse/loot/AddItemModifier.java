4ebef9841c8a265b9fe4c17ccc62dc8203a20be6
karen-matsunaga <karen.f.matsunaga@gmail.com>
2024-10-18 20:18:56 -0300

    2024-10-18 Clean code AddItemModifier.java file

--- src/main/java/net/karen/mccourse/loot/AddItemModifier.java
+++ src/main/java/net/karen/mccourse/loot/AddItemModifier.java
@@ -22,63 +22,49 @@ import java.util.function.Supplier;
 public class AddItemModifier extends LootModifier {
     public static final Supplier<Codec<AddItemModifier>> CODEC = Suppliers.memoize(() ->
             RecordCodecBuilder.create(inst -> codecStart(inst)
-                    .and(ForgeRegistries.ITEMS.getCodec().listOf().fieldOf("items").forGetter(m -> m.items))
-                    .apply(inst, AddItemModifier::new)));
+                    .and(ForgeRegistries.ITEMS.getCodec().listOf().fieldOf("items").forGetter(m -> m.items)).apply(inst, AddItemModifier::new)));
 
     private final List<Item> items;
 
-    public AddItemModifier(LootItemCondition[] conditionsIn, List<Item> items) {
-        super(conditionsIn);
-        this.items = items;
-    }
+    public AddItemModifier(LootItemCondition[] conditionsIn, List<Item> items) { super(conditionsIn); this.items = items; }
 
     @Override
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        generatedLoot.clear();
-        ItemStack tool = context.getParamOrNull(LootContextParams.TOOL);  // Get the tool used by the player
-
-        if (tool != null && tool.getEnchantmentLevel(Enchantments.SILK_TOUCH) > 0) { // Check for Silk Touch enchantment
+        if (context.getParamOrNull(LootContextParams.TOOL) != null && context.getParamOrNull(LootContextParams.TOOL).getEnchantmentLevel(Enchantments.SILK_TOUCH) > 0) {
             for (Item item : items) {  // Loop through each item in the list
-                if (context.getQueriedLootTableId().equals(Blocks.DIAMOND_ORE.getLootTable())) { // If mined DIAMOND ORE
-                        generatedLoot.add(new ItemStack(item));
-                } else if (context.getQueriedLootTableId().equals(Blocks.ANCIENT_DEBRIS.getLootTable())) { // If mined ANCIENT DEBRIS
-                        generatedLoot.add(new ItemStack(item));
+                if (context.getQueriedLootTableId().equals(Blocks.DIAMOND_ORE.getLootTable())) { // If mined DIAMOND ORE with Silk Touch's enchantment
+                        generatedLoot.add(new ItemStack(item)); // Drop itself
+                } else if (context.getQueriedLootTableId().equals(Blocks.ANCIENT_DEBRIS.getLootTable())) { // If mined ANCIENT DEBRIS with Silk Touch's enchantment
+                        generatedLoot.add(new ItemStack(item)); // Drop itself
                 }
             }
         }
 
-        // Check for Fortune enchantment
-        if (tool != null && tool.getEnchantmentLevel(Enchantments.BLOCK_FORTUNE) > 0) {
-            int fortuneLevel = tool.getEnchantmentLevel(Enchantments.BLOCK_FORTUNE); // Fortune's enchantment level
+        if (context.getParamOrNull(LootContextParams.TOOL) != null && context.getParamOrNull(LootContextParams.TOOL).getEnchantmentLevel(Enchantments.BLOCK_FORTUNE) > 0) {
+            int fortuneLevel = context.getParamOrNull(LootContextParams.TOOL).getEnchantmentLevel(Enchantments.BLOCK_FORTUNE); // Fortune's enchantment level
             int drops = context.getRandom().nextInt(fortuneLevel) + UniformGenerator.between(1.0f, 2.0f).getInt(context); // Drops randomly
 
             for (Item item : items) {  // Loop through each item in the list
-                if (context.getQueriedLootTableId().equals(Blocks.DIAMOND_ORE.getLootTable())) { // If mined DIAMOND ORE
+                if (context.getQueriedLootTableId().equals(Blocks.DIAMOND_ORE.getLootTable())) { // If mined DIAMOND ORE with Fortune's enchantment
                         generatedLoot.add(new ItemStack(item, drops)); // Drop multiple diamond's ores and diamonds
-                } else if (context.getQueriedLootTableId().equals(Blocks.ANCIENT_DEBRIS.getLootTable())) { // If mined ANCIENT DEBRIS
+                } else if (context.getQueriedLootTableId().equals(Blocks.ANCIENT_DEBRIS.getLootTable())) { // If mined ANCIENT DEBRIS with Fortune's enchantment
                         generatedLoot.add(new ItemStack(item, drops)); // Drop multiple ancient debris's ores and netherite scraps
                 }
             }
         }
 
         // Apply loot conditions and add each item in the list
-        for (LootItemCondition condition : this.conditions) {
-            if (!condition.test(context)) {
-                return generatedLoot;
-            }
-        }
+        for (LootItemCondition condition : this.conditions) { if (!condition.test(context)) { return generatedLoot; } }
 
         // No Silk Touch or Fortune - add each item in the list normally and update the data and return the list
-        for (Item item : items) {
-            generatedLoot.add(new ItemStack(item));
-        }
+        for (Item item : items) { generatedLoot.add(new ItemStack(item)); }
 
-        // Return normal loot modifier even if to exist 1000 items
-        return generatedLoot;
+        return generatedLoot; // Return normal loot modifier even if to exist 1000 items
     }
 
     @Override
     public Codec<? extends IGlobalLootModifier> codec() {
         return CODEC.get();
     }
+
 }
