cc40b30765c16aabf3133e416d98af6cbb6ba879
karen-matsunaga <karen.f.matsunaga@gmail.com>
2024-10-19 14:18:29 -0300

    2024-10-19 Adding custom Villager trades

--- src/main/java/net/karen/mccourse/event/ModEvents.java
+++ src/main/java/net/karen/mccourse/event/ModEvents.java
@@ -1,5 +1,6 @@
 package net.karen.mccourse.event;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.karen.mccourse.MCCourseMod;
 import net.karen.mccourse.command.ReturnHomeCommand;
 import net.karen.mccourse.command.SetHomeCommand;
@@ -9,62 +10,56 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.command.ConfigCommand;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Mod.EventBusSubscriber(modid = MCCourseMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ModEvents {
-
-    // Hammer's tool EVENT
     // Done with the help of https://github.com/CoFH/CoFHCore/blob/1.19.x/src/main/java/cofh/core/event/AreaEffectEvents.java
     // Don't be a jerk License
-    private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
+    private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>(); // Hammer's tool EVENT
     @SubscribeEvent
     public static void onHammerUsage(BlockEvent.BreakEvent event) {
-        // Player is using Hammer tool
-        Player player = event.getPlayer();
+        Player player = event.getPlayer(); // Player is using Hammer tool
         ItemStack mainHandItem = player.getMainHandItem();
 
-        // If player destroyed a block with Hammer tool
-        if(mainHandItem.getItem() instanceof HammerItem hammer && player instanceof ServerPlayer serverPlayer) {
+        if(mainHandItem.getItem() instanceof HammerItem hammer && player instanceof ServerPlayer serverPlayer) { // If player destroyed a block with Hammer tool
             BlockPos initalBlockPos = event.getPos();
-            if (HARVESTED_BLOCKS.contains(initalBlockPos)) {
-                return;
-            }
+            if (HARVESTED_BLOCKS.contains(initalBlockPos)) { return; }
 
-            // Player's position to break a block with Hammer tool
-            for (BlockPos pos : HammerItem.getBlocksToBeDestroyed(1, initalBlockPos, serverPlayer)) {
-                if(pos == initalBlockPos || !hammer.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(pos))) {
-                    continue;
-                }
+            for (BlockPos pos : HammerItem.getBlocksToBeDestroyed(1, initalBlockPos, serverPlayer)) { // Player's position to break a block with Hammer tool
+                if(pos == initalBlockPos || !hammer.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(pos))) { continue; }
 
-            // Player destroyed block with Hammer tool
                 // Have to add them to a Set otherwise, the same code right here will get called for each block!
-                HARVESTED_BLOCKS.add(pos);
+                HARVESTED_BLOCKS.add(pos); // Player destroyed block with Hammer tool
                 serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
             }
         }
     }
 
-    // Home's commands EVENT
-    // Register all custom commands
+    // CUSTOM EVENT - Home's commands EVENT
     @SubscribeEvent
-    public static void onCommandsRegister(RegisterCommandsEvent event) {
+    public static void onCommandsRegister(RegisterCommandsEvent event) { // Register all custom commands
         new SetHomeCommand(event.getDispatcher());
         new ReturnHomeCommand(event.getDispatcher());
-
         ConfigCommand.register(event.getDispatcher());
     }
 
@@ -80,9 +75,9 @@ public class ModEvents {
     public static void livingDamage(LivingDamageEvent event) {
         if(event.getEntity() instanceof Sheep) {
             if(event.getSource().getDirectEntity() instanceof Player player) {
-                if(player.getItemInHand(InteractionHand.MAIN_HAND).getItem() == ModItems.ALEXANDRITE_AXE.get()) {
+                if (player.getItemInHand(InteractionHand.MAIN_HAND).getItem() == ModItems.ALEXANDRITE_AXE.get()) {
                     MCCourseMod.LOGGER.info("Sheep was hit with Alexandrite Axe by " + player.getName().getString());
-                } else if(player.getItemInHand(InteractionHand.MAIN_HAND).getItem() == Items.DIAMOND) {
+                } else if (player.getItemInHand(InteractionHand.MAIN_HAND).getItem() == Items.DIAMOND) {
                     MCCourseMod.LOGGER.info("Sheep was hit with DIAMOND by " + player.getName().getString());
                 } else {
                     MCCourseMod.LOGGER.info("Sheep was hit with something else by " + player.getName().getString());
@@ -91,4 +86,52 @@ public class ModEvents {
         }
     }
 
+    // CUSTOM EVENT - Custom Villager's professions trade
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        // Villager's farm profession
+        if (event.getType() == VillagerProfession.FARMER) {
+            // List of all trades
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+
+            // List of all trades that the player can trade
+            ItemStack stack = new ItemStack(ModItems.KOHLRABI.get(), 6);  // Received KOHLRABI with Villager's level 1
+            int villagerLevel = 1;
+            trades.get(villagerLevel).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2), stack, 10, 2, 0.02f));
+
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(ModItems.KOHLRABI_SEEDS.get()), 3, 2, 0.02f));
+        }
+
+        // Villager's farm profession
+        if (event.getType() == VillagerProfession.TOOLSMITH) {
+            // List of all trades
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+
+            // List of all trades that the player can trade
+            ItemStack stack = new ItemStack(ModItems.ALEXANDRITE_PAXEL.get(), 1); // Received ALEXANDRITE PAXEL with Villager's level 3
+            int villagerLevel = 3;
+            trades.get(villagerLevel).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 12), stack, 2, 5, 0.06f));
+        }
+    }
+
+    // CUSTOM EVENT - Custom Villager Wandering
+    @SubscribeEvent
+    public static void addWanderingTrades(WandererTradesEvent event) {
+        // List of all trades that the player can trade - Generic and Rare trades
+        List<VillagerTrades.ItemListing> genericTrades = event.getGenericTrades();
+        List<VillagerTrades.ItemListing> rareTrades = event.getRareTrades();
+
+        // List of all generic and rare trades
+        ItemStack stack = new ItemStack(ModItems.KOHLRABI.get(), 6);
+        genericTrades.add((pTrader, pRandom) -> new MerchantOffer(
+                new ItemStack(Items.EMERALD, 2), stack, 10, 2, 0.02f));
+
+        rareTrades.add((pTrader, pRandom) -> new MerchantOffer(
+                new ItemStack(Items.EMERALD, 5),
+                new ItemStack(ModItems.KOHLRABI_SEEDS.get()), 3, 2, 0.02f));
+    }
 }
