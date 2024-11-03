df609c95b2c1178acd0ceb24f4774a6aef6b193d
karen matsunaga <Karen.f.matsunaga@gmail.com>
2024-11-03 17:38:14 -0300

    2024-11-03 Custom mob sounds

--- src/main/java/net/karen/mccourse/entity/custom/RhinoEntity.java
+++ src/main/java/net/karen/mccourse/entity/custom/RhinoEntity.java
@@ -9,7 +9,10 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -126,4 +129,14 @@ public class RhinoEntity extends Animal {
         super.addAdditionalSaveData(pCompound);
         pCompound.putInt("Variant", this.getTypeVariant());
     }
+
+    /* SOUNDS */
+    @Override
+    protected @Nullable SoundEvent getAmbientSound() { return SoundEvents.HOGLIN_AMBIENT; }
+
+    @Override
+    protected @Nullable SoundEvent getHurtSound(DamageSource pDamageSource) { return SoundEvents.RAVAGER_HURT; }
+
+    @Override
+    protected @Nullable SoundEvent getDeathSound() { return SoundEvents.DOLPHIN_DEATH; }
 }
