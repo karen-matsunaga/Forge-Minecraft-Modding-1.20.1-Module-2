29f20a577cf6c3193e0ca9c62c2e3470bf0f0d38
karen matsunaga <Karen.f.matsunaga@gmail.com>
2024-11-14 09:38:05 -0300

    2024-11-14 Improved Pink's helmet custom armor effects, and description of code

--- src/main/java/net/karen/mccourse/item/custom/ModHelmetArmorItem.java
+++ src/main/java/net/karen/mccourse/item/custom/ModHelmetArmorItem.java
@@ -15,33 +15,25 @@ import java.util.List;
 import java.util.Map;
 
 public class ModHelmetArmorItem extends ArmorItem {
-    // Specific armor material to mob effect instance that applied in player
+    // Specific Helmet armor material to mob effect instance that applied in player
     private static final Map<ArmorMaterial, List<MobEffectInstance>> MATERIAL_TO_EFFECT_MAP =
             (new ImmutableMap.Builder<ArmorMaterial, List<MobEffectInstance>>())
+                    // List of effects or custom effects added on player if used custom helmet armor specified
                     .put(ModArmorMaterials.PINK, Arrays.asList(
-                            // Glowing effect
-                            new MobEffectInstance(MobEffects.GLOWING, 200, 2, false, false, true),
-                            // Strength effect
-                            new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 2, false, false, true),
-                            // Night vision effect
-                            new MobEffectInstance(MobEffects.NIGHT_VISION, 200, 2, false, false, true)
-                    ))
-                    .build();
+                            new MobEffectInstance(MobEffects.GLOWING, 300, 1, false, false, true),
+                            new MobEffectInstance(MobEffects.NIGHT_VISION, 300, 1, false, false, true))).build();
 
     public ModHelmetArmorItem(ArmorMaterial material, Type type, Properties properties) { super(material, type, properties); }
 
     // Apply effect if player using all parts of armor
     @Override
-    public void onArmorTick(ItemStack stack, Level level, Player player) {
-        if (!level.isClientSide() && hasHelmetOn(player)) { evaluateArmorEffects(player); }
-    }
+    public void onArmorTick(ItemStack stack, Level level, Player player) { if (!level.isClientSide() && hasHelmetOn(player)) { evaluateArmorEffects(player); } }
 
-    /// If player is using same armor material applies all effects
+    // If player is using same armor material applies all effects
     private void evaluateArmorEffects(Player player) {
         for (Map.Entry<ArmorMaterial, List<MobEffectInstance>> entry : MATERIAL_TO_EFFECT_MAP.entrySet()) {
             ArmorMaterial mapArmorMaterial = entry.getKey();
             List<MobEffectInstance> effects = entry.getValue();
-
             if (hasPlayerCorrectArmorOn(mapArmorMaterial, player)) { for (MobEffectInstance effect : effects) { addEffectToPlayer(player, effect); } }
         }
     }
@@ -50,18 +42,17 @@ public class ModHelmetArmorItem extends ArmorItem {
     private void addEffectToPlayer(Player player, MobEffectInstance effect) {
         boolean hasPlayerEffect = player.hasEffect(effect.getEffect());
 
-        if (!hasPlayerEffect) {
-            player.addEffect(new MobEffectInstance(effect.getEffect(),
-                    effect.getDuration(), effect.getAmplifier()));
-        }
+        // Player has received effects and ticks not decreased while exist helmet on armor slot
+        if (effect.getDuration() == 300 && hasPlayerEffect) { player.addEffect(new MobEffectInstance(effect.getEffect(), effect.getDuration(), effect.getAmplifier())); }
+
+        // Player not received effects
+        else if (!hasPlayerEffect) { player.addEffect(new MobEffectInstance(effect.getEffect(), effect.getDuration(), effect.getAmplifier())); }
     }
 
-    // If player using same armor material
+    // If player using same Helmet armor material
     private boolean hasPlayerCorrectArmorOn(ArmorMaterial mapArmorMaterial, Player player) {
-        if (!(player.getInventory().getArmor(3).getItem() instanceof ArmorItem helmet)) { return false; }
-        return helmet.getMaterial() == mapArmorMaterial;
-    }
+        if (!(player.getInventory().getArmor(3).getItem() instanceof ArmorItem helmet)) { return false; } return helmet.getMaterial() == mapArmorMaterial; }
 
-    // If player using Helmet part
-    private boolean hasHelmetOn(Player player) { ItemStack helmet = player.getInventory().getArmor(3); return !helmet.isEmpty(); }
+    // If player using Helmet armor slot 3 part
+    private boolean hasHelmetOn(Player player) { return !player.getInventory().getArmor(3).isEmpty(); }
 }
