ea52fb44a38d247fed62999aaca9830168148a42
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-03-31 20:07:43 -0300

    2025-03-31 Fixed Fly custom effect

--- src/main/java/net/karen/mccourse/effect/FlyEffect.java
+++ src/main/java/net/karen/mccourse/effect/FlyEffect.java
@@ -5,31 +5,30 @@ import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.player.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class FlyEffect extends MobEffect {
     protected FlyEffect(MobEffectCategory pCategory, int pColor) { super(pCategory, pColor); }
 
-    // Fly effect applied
     @Override
-    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+    public void applyEffectTick(@NotNull LivingEntity pLivingEntity, int pAmplifier) {
         if (pLivingEntity instanceof Player player) {
-            player.getAbilities().mayfly = true; // Allow flight
+            player.getAbilities().mayfly = player.hasEffect(ModEffects.FLY_EFFECT.get()); // Allow flight
             player.onUpdateAbilities(); // Update the player abilities
         }
         super.applyEffectTick(pLivingEntity, pAmplifier);
-    }
+    } // Fly effect applied
 
-    // Fly effect ended
     @Override
-    public void removeAttributeModifiers(LivingEntity pLivingEntity, AttributeMap pAttributeMap, int pAmplifier) {
+    public void removeAttributeModifiers(@NotNull LivingEntity pLivingEntity, @NotNull AttributeMap pAttributeMap, int pAmplifier) {
         if (pLivingEntity instanceof Player player) {
             player.getAbilities().mayfly = false;
             player.getAbilities().flying = false;
             player.onUpdateAbilities(); // Update the player abilities
         }
         super.removeAttributeModifiers(pLivingEntity, pAttributeMap, pAmplifier);
-    }
+    } // Fly effect ended
 
     @Override
-    public boolean isDurationEffectTick(int pDuration, int pAmplifier) { return true; } // Fly effect duration
+    public boolean isDurationEffectTick(int pDuration, int pAmplifier) { return pDuration % 40 == 0; } // Fly effect duration
 }
