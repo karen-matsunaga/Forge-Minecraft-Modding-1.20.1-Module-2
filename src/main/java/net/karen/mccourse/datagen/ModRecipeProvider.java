c8c92863159f3d0109add8c063c870a57f9437d8
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-04-06 17:35:20 -0300

    2025-04-06 Added tools, armors and blocks recipes

--- src/main/java/net/karen/mccourse/datagen/ModRecipeProvider.java
+++ src/main/java/net/karen/mccourse/datagen/ModRecipeProvider.java
@@ -30,7 +30,7 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     // Create all recipes
     @Override
     protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
-        // Alexandrite
+        // Alexandrite transforms on Alexandrite Block recipe
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.ALEXANDRITE_BLOCK.get())
                 .pattern("AAA")
                 .pattern("AAA")
@@ -40,7 +40,7 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                         of(ModItems.ALEXANDRITE.get()).build()))
                 .save(pWriter);
 
-        // Alexandrite Block
+        // Alexandrite Block converts to Alexandrite recipe
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.ALEXANDRITE.get(), 9)
                 .requires(ModBlocks.ALEXANDRITE_BLOCK.get())
                 .unlockedBy("has_alexandrite_block", inventoryTrigger(ItemPredicate.Builder.item().
@@ -65,6 +65,27 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         new GemEmpoweringRecipeBuilder(Items.COAL, Items.DIAMOND, 7, 40, 150,
                 new FluidStack(Fluids.LAVA, 500))
                 .unlockedBy("has_diamond", has(Items.DIAMOND)).save(pWriter);
+
+        // My custom mod
+        // Hammer and Pickaxe
+        swordItem(ModItems.ALEXANDRITE_SWORD.get(), ModItems.ALEXANDRITE.get(), pWriter); // Sword
+
+        pickaxeHammerItem(ModItems.ALEXANDRITE_HAMMER.get(), ModBlocks.ALEXANDRITE_BLOCK.get(), pWriter); // Hammer
+        pickaxeHammerItem(ModItems.ALEXANDRITE_PICKAXE.get(), ModItems.ALEXANDRITE.get(), pWriter); // Pickaxe
+
+        axeItem(ModItems.ALEXANDRITE_AXE.get(), ModItems.ALEXANDRITE.get(), pWriter); // Axe
+        shovelItem(ModItems.ALEXANDRITE_SHOVEL.get(), ModItems.ALEXANDRITE.get(), pWriter); // Shovel
+
+        paxelItem(ModItems.ALEXANDRITE_PAXEL.get(), ModItems.ALEXANDRITE_PICKAXE.get(),
+                ModItems.ALEXANDRITE_AXE.get(), ModItems.ALEXANDRITE_SHOVEL.get(), pWriter); // Paxel
+
+        hoeItem(ModItems.ALEXANDRITE_HOE.get(), ModItems.ALEXANDRITE.get(), pWriter); // Hoe
+
+        // Armor
+        helmetArmor(ModItems.ALEXANDRITE_HELMET.get(), ModItems.ALEXANDRITE.get(), pWriter); // Helmet
+        chestplateArmor(ModItems.ALEXANDRITE_CHESTPLATE.get(), ModItems.ALEXANDRITE.get(), pWriter); // Chestplate
+        leggingsArmor(ModItems.ALEXANDRITE_LEGGINGS.get(), ModItems.ALEXANDRITE.get(), pWriter); // Leggings
+        bootsArmor(ModItems.ALEXANDRITE_BOOTS.get(), ModItems.ALEXANDRITE.get(), pWriter); // Boots
     }
 
     // Smelting
@@ -89,5 +110,123 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                             pCookingSerializer).group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
                     .save(pFinishedRecipeConsumer, MCCourseMod.MOD_ID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
         }
+    }
+
+    // Custom recipe
+    protected static void pickaxeHammerItem(ItemLike pResult, ItemLike item, Consumer<FinishedRecipe> pWriter) {
+        // Hammer and Pickaxe tools
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern("AAA")
+                .pattern(" B ")
+                .pattern(" B ")
+                .define('A', item)
+                .define('B', Items.STICK)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
+    }
+
+    protected static void helmetArmor(ItemLike pResult, ItemLike item, Consumer<FinishedRecipe> pWriter) {
+        // Helmet
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern("AAA")
+                .pattern("A A")
+                .pattern("   ")
+                .define('A', item)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
+    }
+
+    protected static void chestplateArmor(ItemLike pResult, ItemLike item, Consumer<FinishedRecipe> pWriter) {
+        // Chestplate
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern("A A")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', item)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
+    }
+
+    protected static void leggingsArmor(ItemLike pResult, ItemLike item, Consumer<FinishedRecipe> pWriter) {
+        // Leggings
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern("AAA")
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', item)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
+    }
+
+    protected static void bootsArmor(ItemLike pResult, ItemLike item, Consumer<FinishedRecipe> pWriter) {
+        // Boots
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern("   ")
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', item)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
+    }
+
+    protected static void shovelItem(ItemLike pResult, ItemLike item, Consumer<FinishedRecipe> pWriter) {
+        // Shovel
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern(" A ")
+                .pattern(" B ")
+                .pattern(" B ")
+                .define('A', item)
+                .define('B', Items.STICK)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
+    }
+
+    protected static void axeItem(ItemLike pResult, ItemLike item, Consumer<FinishedRecipe> pWriter) {
+        // Axe
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern("AA ")
+                .pattern("AB ")
+                .pattern(" B ")
+                .define('A', item)
+                .define('B', Items.STICK)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
+    }
+
+    protected static void paxelItem(ItemLike pResult, ItemLike item, ItemLike item2, ItemLike item3, Consumer<FinishedRecipe> pWriter) {
+        // Paxel
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern("   ")
+                .pattern("ABC")
+                .pattern("   ")
+                .define('A', item)
+                .define('B', item2)
+                .define('C', item3)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
+    }
+
+    protected static void swordItem(ItemLike pResult, ItemLike item, Consumer<FinishedRecipe> pWriter) {
+        // Paxel
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern(" A ")
+                .pattern(" A ")
+                .pattern(" B ")
+                .define('A', item)
+                .define('B', Items.STICK)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
+    }
+
+    protected static void hoeItem(ItemLike pResult, ItemLike item, Consumer<FinishedRecipe> pWriter) {
+        // Paxel
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, pResult, 1)
+                .pattern("AA ")
+                .pattern(" B ")
+                .pattern(" B ")
+                .define('A', item)
+                .define('B', Items.STICK)
+                .unlockedBy("has_item", has(pResult))
+                .save(pWriter);
     }
 }
