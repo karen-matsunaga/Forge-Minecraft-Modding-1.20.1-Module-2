7a5fe3731dea1a5a8e8ab5d8f5cbc77c6838f60d
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-05-06 10:50:07 -0300

    2025-05-06 Fixed Mccourse Generator block drop problems

--- src/main/java/net/karen/mccourse/block/custom/MccourseGeneratorBlock.java
+++ src/main/java/net/karen/mccourse/block/custom/MccourseGeneratorBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -20,19 +21,27 @@ public class MccourseGeneratorBlock extends Block {
     }
 
     @Override
-    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos,
-                                       Player player, boolean willHarvest, FluidState fluid) {
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player,
+                                       boolean willHarvest, FluidState fluid) {
         if (!level.isClientSide) {
-            if (player.isCreative()) { return true; }
+            if (player.isCreative()) {
+                level.destroyBlock(pos, false); // Destroyed block
+                return true;
+            } // Enables destruction in the creative
             else {
-                for (Item item : items) { // Items generated
+                for (Item item : items) { // Drop custom items only in survival
                     ItemStack stack = new ItemStack(item);
                     Containers.dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), stack);
                 }
-                level.sendBlockUpdated(pos, state, state, 3);
+                level.sendBlockUpdated(pos, state, state, 3); // Reseat the block - Prevents destruction outside of creative
                 return false;
             }
         }
-        return player.isCreative(); // Prevents the block from being destroyed
+        return player.isCreative(); // On the client side, it allows only if you are creative
+    }
+
+    @Override
+    public void playerWillDestroy(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull Player player) {
+        super.playerWillDestroy(level, pos, state, player);
     }
 }
