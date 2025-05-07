6e57126b70c047ca1c019fbdbfdc1fe5512a84c2
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-05-06 21:46:54 -0300

    2025-05-06 Added the functionality to break the Mccourse Generator only with item Pink Pickaxe

--- src/main/java/net/karen/mccourse/block/custom/MccourseGeneratorBlock.java
+++ src/main/java/net/karen/mccourse/block/custom/MccourseGeneratorBlock.java
@@ -1,15 +1,16 @@
 package net.karen.mccourse.block.custom;
 
+import net.karen.mccourse.item.ModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Containers;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -23,25 +24,28 @@ public class MccourseGeneratorBlock extends Block {
     @Override
     public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player,
                                        boolean willHarvest, FluidState fluid) {
+        ItemStack heldItem = player.getMainHandItem();
         if (!level.isClientSide) {
-            if (player.isCreative()) {
-                level.destroyBlock(pos, false); // Destroyed block
-                return true;
-            } // Enables destruction in the creative
+            if (player.isCreative() || heldItem.is(ModItems.PINK_PICKAXE.get())) {
+                // Destroyed block, but not received drop block
+                return level.destroyBlock(pos, false); // Enables destruction in the creative
+            }
             else {
                 for (Item item : items) { // Drop custom items only in survival
                     ItemStack stack = new ItemStack(item);
-                    Containers.dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), stack);
+                    Containers.dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), stack); // Item generated
                 }
                 level.sendBlockUpdated(pos, state, state, 3); // Reseat the block - Prevents destruction outside of creative
                 return false;
             }
         }
-        return player.isCreative(); // On the client side, it allows only if you are creative
+        return player.isCreative() || heldItem.is(ModItems.PINK_PICKAXE.get()); // On the client side, it allows only if you are creative
     }
 
     @Override
-    public void playerWillDestroy(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull Player player) {
-        super.playerWillDestroy(level, pos, state, player);
+    public boolean canHarvestBlock(BlockState state, BlockGetter level, BlockPos pos, Player player) {
+        // Checks if the player is holding the specific item
+        ItemStack heldItem = player.getMainHandItem();
+        return heldItem.is(ModItems.PINK_PICKAXE.get()); // Change to your item
     }
 }
