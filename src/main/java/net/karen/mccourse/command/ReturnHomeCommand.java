d5721580f7f392da21f9ab04373e94cf018bd96f
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-05-17 11:28:39 -0300

    2025-05-17 Fixed Home custom command comments and code

--- src/main/java/net/karen/mccourse/command/ReturnHomeCommand.java
+++ src/main/java/net/karen/mccourse/command/ReturnHomeCommand.java
@@ -9,29 +9,34 @@ import net.minecraft.server.level.ServerPlayer;
 
 public class ReturnHomeCommand {
     public ReturnHomeCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
+        // Command to type on chat -> /home return
         dispatcher.register(Commands.literal("home").then(Commands.literal("return").executes(this::execute)));
     }
 
     // When player to type this command it is returned on home
     private int execute(CommandContext<CommandSourceStack> context) {
         ServerPlayer player = context.getSource().getPlayer();
+        // Initialize value with 0
         int value = 0;
         if (player != null) {
             boolean hasHomepos = player.getPersistentData().getIntArray("mccourse.homepos").length != 0;
-            // If not saved anyone set home
+            // If there is no Set Home
             if (!hasHomepos) {
+                // Display the message in the chat (FAIL)
                 context.getSource().sendFailure(Component.literal("No Home Position has been set!"));
-                value = -1;
+                value = -1; // Player's position not save (FALSE)
             }
-            // If exists some set home
+            // The Player will return to the saved position
             else {
+                // Get the Player's saved [X, Y, Z] coordinates from the /home set COMMAND
                 int[] playerPos = player.getPersistentData().getIntArray("mccourse.homepos");
                 player.teleportTo(playerPos[0], playerPos[1], playerPos[2]);
 
+                // Display the message in the chat (SUCCESS)
                 context.getSource().sendSuccess(() -> Component.literal("Player returned Home!"), false);
-                value = 1;
+                value = 1; // Player's position save with success (TRUE)
             }
         }
-        return value;
+        return value; // Depends on result (1 - TRUE) or (-1 - FALSE)
     }
 }
--- src/main/java/net/karen/mccourse/command/SetHomeCommand.java
+++ src/main/java/net/karen/mccourse/command/SetHomeCommand.java
@@ -4,12 +4,12 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 
 public class SetHomeCommand {
     public SetHomeCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
+        // Command to type on chat -> /home set
         dispatcher.register(Commands.literal("home").then(Commands.literal("set")
                 .executes(this::execute)));
     }
@@ -18,17 +18,19 @@ public class SetHomeCommand {
     private int execute(CommandContext<CommandSourceStack> context) {
         ServerPlayer player = context.getSource().getPlayer();
         if (player != null) {
-            BlockPos playerPos = player.blockPosition();
-
-            // Player's position
-            String positionString = "[" + playerPos.getX() + ", " + playerPos.getY() + ", " + playerPos.getZ() + "]";
+            // Player's [X, Y, Z] block positions
+            int x = player.blockPosition().getX();
+            int y = player.blockPosition().getY();
+            int z = player.blockPosition().getZ();
 
             // Save data of Player's position
             player.getPersistentData().putIntArray("mccourse.homepos",
-                    new int[] {playerPos.getX(), playerPos.getY(), playerPos.getZ()});
+                    new int[] { x, y, z });
 
-            context.getSource().sendSuccess(() -> Component.literal("Set home at " + positionString), true);
+            // Display the message in the chat
+            context.getSource().sendSuccess(() -> Component.literal("Set home at " +
+                    "[" + x + ", " + y + ", " + z + "]"), true);
         }
-        return 1;
+        return 1; // Player's position save with success
     }
 }
