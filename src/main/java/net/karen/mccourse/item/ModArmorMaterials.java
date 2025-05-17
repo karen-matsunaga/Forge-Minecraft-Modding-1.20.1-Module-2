ec58d34f8756f596c5e15b9c576c428da0c4bdaa
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-05-17 15:34:03 -0300

    2025-05-17 Improved ModArmorMaterials code

--- src/main/java/net/karen/mccourse/item/ModArmorMaterials.java
+++ src/main/java/net/karen/mccourse/item/ModArmorMaterials.java
@@ -10,42 +10,48 @@ import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.EnumMap;
 import java.util.function.Supplier;
 
 public enum ModArmorMaterials implements ArmorMaterial {
-
     // Alexandrite's armor material
-    ALEXANDRITE("alexandrite", 24, Util.make(new EnumMap<>(ArmorItem.Type.class), (p_266655_) -> {
-        p_266655_.put(ArmorItem.Type.BOOTS, 4);
-        p_266655_.put(ArmorItem.Type.LEGGINGS, 5);
-        p_266655_.put(ArmorItem.Type.CHESTPLATE, 7);
-        p_266655_.put(ArmorItem.Type.HELMET, 5);
-    }), 15, SoundEvents.ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, () -> Ingredient.of(ModItems.ALEXANDRITE.get())),
+    ALEXANDRITE("alexandrite", 24, Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+        map.put(ArmorItem.Type.BOOTS, 4);
+        map.put(ArmorItem.Type.LEGGINGS, 5);
+        map.put(ArmorItem.Type.CHESTPLATE, 7);
+        map.put(ArmorItem.Type.HELMET, 5);
+    }), 15, SoundEvents.ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F,
+            () -> Ingredient.of(ModItems.ALEXANDRITE.get())),
 
     // My custom armor material
-    PINK("pink", 24, Util.make(new EnumMap<>(ArmorItem.Type.class), (p_266655_) -> {
-        p_266655_.put(ArmorItem.Type.BOOTS, 4);
-        p_266655_.put(ArmorItem.Type.LEGGINGS, 5);
-        p_266655_.put(ArmorItem.Type.CHESTPLATE, 7);
-        p_266655_.put(ArmorItem.Type.HELMET, 5);
-    }), 15, SoundEvents.ARMOR_EQUIP_DIAMOND, 3.0F, 0.1F, () -> Ingredient.of(ModItems.PINK.get())),
+    PINK("pink", 24, Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+        map.put(ArmorItem.Type.BOOTS, 4);
+        map.put(ArmorItem.Type.LEGGINGS, 5);
+        map.put(ArmorItem.Type.CHESTPLATE, 7);
+        map.put(ArmorItem.Type.HELMET, 5);
+    }), 15, SoundEvents.ARMOR_EQUIP_DIAMOND, 3.0F, 0.1F,
+            () -> Ingredient.of(ModItems.PINK.get())),
 
-    COPPER("copper", 24, Util.make(new EnumMap<>(ArmorItem.Type.class), (p_266655_) -> {
-        p_266655_.put(ArmorItem.Type.BOOTS, 4);
-        p_266655_.put(ArmorItem.Type.LEGGINGS, 5);
-        p_266655_.put(ArmorItem.Type.CHESTPLATE, 7);
-        p_266655_.put(ArmorItem.Type.HELMET, 5);
-    }), 15, SoundEvents.ARMOR_EQUIP_DIAMOND, 3.0F, 0.1F, () -> Ingredient.of(Items.COPPER_INGOT)),
+    COPPER("copper", 24, Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+        map.put(ArmorItem.Type.BOOTS, 4);
+        map.put(ArmorItem.Type.LEGGINGS, 5);
+        map.put(ArmorItem.Type.CHESTPLATE, 7);
+        map.put(ArmorItem.Type.HELMET, 5);
+    }), 15, SoundEvents.ARMOR_EQUIP_DIAMOND, 3.0F, 0.1F,
+            () -> Ingredient.of(Items.COPPER_INGOT)),
     ; // Custom armor
 
-    public static final StringRepresentable.EnumCodec<ArmorMaterials> CODEC = StringRepresentable.fromEnum(ArmorMaterials::values);
-    private static final EnumMap<ArmorItem.Type, Integer> HEALTH_FUNCTION_FOR_TYPE = Util.make(new EnumMap<>(ArmorItem.Type.class), (p_266653_) -> {
-        p_266653_.put(ArmorItem.Type.BOOTS, 13);
-        p_266653_.put(ArmorItem.Type.LEGGINGS, 15);
-        p_266653_.put(ArmorItem.Type.CHESTPLATE, 16);
-        p_266653_.put(ArmorItem.Type.HELMET, 11);
+    public static final StringRepresentable.EnumCodec<ArmorMaterials> CODEC =
+            StringRepresentable.fromEnum(ArmorMaterials::values);
+
+    private static final EnumMap<ArmorItem.Type, Integer> HEALTH_FUNCTION_FOR_TYPE = Util.make(
+            new EnumMap<>(ArmorItem.Type.class), (enumMap) -> {
+        enumMap.put(ArmorItem.Type.BOOTS, 13);
+        enumMap.put(ArmorItem.Type.LEGGINGS, 15);
+        enumMap.put(ArmorItem.Type.CHESTPLATE, 16);
+        enumMap.put(ArmorItem.Type.HELMET, 11);
     });
 
     private final String name;
@@ -57,8 +63,10 @@ public enum ModArmorMaterials implements ArmorMaterial {
     private final float knockbackResistance;
     private final Supplier<Ingredient> repairIngredient;
 
-    ModArmorMaterials(String name, int durabilityMultiplier, EnumMap<ArmorItem.Type, Integer> protectionTypeMap,
-                      int enchantmentValue, SoundEvent soundEvent, float toughness, float knockbackRes, Supplier<Ingredient> repairMaterial) {
+    ModArmorMaterials(String name, int durabilityMultiplier,
+                      EnumMap<ArmorItem.Type, Integer> protectionTypeMap, int enchantmentValue,
+                      SoundEvent soundEvent, float toughness, float knockbackRes,
+                      Supplier<Ingredient> repairMaterial) {
         this.name = name;
         this.durabilityMultiplier = durabilityMultiplier;
         this.protectionFunctionForType = protectionTypeMap;
@@ -70,22 +78,26 @@ public enum ModArmorMaterials implements ArmorMaterial {
     }
 
     // Armor's durability
-    public int getDurabilityForType(ArmorItem.Type p_266745_) { return HEALTH_FUNCTION_FOR_TYPE.get(p_266745_) * this.durabilityMultiplier; }
+    public int getDurabilityForType(ArmorItem.@NotNull Type type) {
+        return HEALTH_FUNCTION_FOR_TYPE.get(type) * this.durabilityMultiplier;
+    }
 
     // Armor's defense
-    public int getDefenseForType(ArmorItem.Type p_266752_) { return this.protectionFunctionForType.get(p_266752_); }
+    public int getDefenseForType(ArmorItem.@NotNull Type type) {
+        return this.protectionFunctionForType.get(type);
+    }
 
     // Armor's enchantment value
     public int getEnchantmentValue() { return this.enchantmentValue; }
 
     // Armor's sound
-    public SoundEvent getEquipSound() { return this.sound; }
+    public @NotNull SoundEvent getEquipSound() { return this.sound; }
 
     // Armor's repair item
-    public Ingredient getRepairIngredient() { return this.repairIngredient.get(); }
+    public @NotNull Ingredient getRepairIngredient() { return this.repairIngredient.get(); }
 
     // Armor's name
-    public String getName() { return MCCourseMod.MOD_ID + ":" + this.name; }
+    public @NotNull String getName() { return MCCourseMod.MOD_ID + ":" + this.name; }
 
     // Armor's toughness
     public float getToughness() { return this.toughness; }
