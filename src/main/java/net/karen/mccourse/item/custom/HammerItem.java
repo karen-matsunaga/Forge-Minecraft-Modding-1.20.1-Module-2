fd4817bd5b5de84c00c304cb4b2989d495ab62f8
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-05-25 17:13:36 -0300

    2025-05-25 Organized Hammer comments and code

--- src/main/java/net/karen/mccourse/item/custom/HammerItem.java
+++ src/main/java/net/karen/mccourse/item/custom/HammerItem.java
@@ -30,15 +30,16 @@ import java.util.List;
 // Credits by Kaupenjoe - https://github.com/Kaupenjoe/Forge-Course-1.20.X/tree/22-customHammer
 // Distributed under MIT - Using with some modifications
 
-// Credits by Alireza Khodakarami (Jiraiyah) - https://github.com/drkhodakarami/uio/blob/master/src/main/java/jiraiyah/uio/item/HammerItem.java
+// Credits by Alireza Khodakarami (Jiraiyah)
+// https://github.com/drkhodakarami/uio/blob/master/src/main/java/jiraiyah/uio/item/HammerItem.java
 // Distributed under MIT - Using with some modifications
 public class HammerItem extends DiggerItem implements Vanishable {
     private final int radius;
     private final boolean infinite;
 
-    public HammerItem(Tier pTier, float pAttackDamageModifier, float pAttackSpeedModifier, TagKey<Block> blockTags,
-                      Properties pProperties, int radius, boolean infinite) {
-        super(pAttackDamageModifier, pAttackSpeedModifier, pTier, blockTags, pProperties);
+    public HammerItem(Tier tier, float pAttackDamageModifier, float pAttackSpeedModifier,
+                      TagKey<Block> blockTags, Properties properties, int radius, boolean infinite) {
+        super(pAttackDamageModifier, pAttackSpeedModifier, tier, blockTags, properties);
         this.radius = radius - 1; // Radius declared on ModItems
         this.infinite = infinite;
     }
@@ -46,9 +47,9 @@ public class HammerItem extends DiggerItem implements Vanishable {
     public int getRadius() { return this.radius; } // Declared radius activated on ModEvents
 
     // Player to receive the blocks destroyed
-    public static List<BlockPos> getBlocksToBeDestroyed(int radius, BlockPos initalBlockPos, ServerPlayer player) {
-        List<BlockPos> positions = new ArrayList<>();
-
+    public static List<BlockPos> getBlocksToBeDestroyed(int radius, BlockPos initalBlockPos,
+                                                        ServerPlayer player) {
+        List<BlockPos> positions = new ArrayList<>(); // Block direction position
         BlockHitResult traceResult = player.level().clip(new ClipContext(player.getEyePosition(1f),
                 (player.getEyePosition(1f).add(player.getViewVector(1f).scale(6f))),
                 ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, player));
@@ -57,37 +58,42 @@ public class HammerItem extends DiggerItem implements Vanishable {
 
         // Directions in that player broken a block - (DOWN/UP, NORTH/SOUTH and EAST/WEST)
         // Check which face of the block was hit to determine the breaking plane
-        if (traceResult.getDirection() == Direction.DOWN || traceResult.getDirection() == Direction.UP) {
-            for (int x = -radius; x <= radius; x++) {
-                for (int y = -radius; y <= radius; y++) {
-                    positions.add(new BlockPos(initalBlockPos.getX() + x, initalBlockPos.getY(), initalBlockPos.getZ() + y));
-                }
-            }
-        }
-
-        if (traceResult.getDirection() == Direction.NORTH || traceResult.getDirection() == Direction.SOUTH) {
-            for (int x = -radius; x <= radius; x++) {
-                for (int y = -radius; y <= radius; y++) {
-                    positions.add(new BlockPos(initalBlockPos.getX() + x, initalBlockPos.getY() + y, initalBlockPos.getZ()));
-                }
-            }
-        }
-
-        if (traceResult.getDirection() == Direction.EAST || traceResult.getDirection() == Direction.WEST) {
-            for (int x = -radius; x <= radius; x++) {
-                for (int y = -radius; y <= radius; y++) {
-                    positions.add(new BlockPos(initalBlockPos.getX(), initalBlockPos.getY() + y, initalBlockPos.getZ() + x));
-                }
-            }
-        }
+        blockPos(traceResult, List.of(Direction.DOWN, Direction.UP), positions, initalBlockPos, radius);
+        blockPos(traceResult, List.of(Direction.NORTH, Direction.SOUTH), positions, initalBlockPos, radius);
+        blockPos(traceResult, List.of(Direction.EAST, Direction.WEST), positions, initalBlockPos, radius);
         return positions;
     }
 
     // Appears tooltip on screen of Hammer.
     @Override
-    public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel, @NotNull List<Component> pTooltipComponents, @NotNull TooltipFlag pIsAdvanced) {
-        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
-        pTooltipComponents.add(Component.translatable("Hammer breaks: " + (this.radius * 2 + 1) + "x" + (this.radius * 2 + 1)));
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level,
+                                @NotNull List<Component> components, @NotNull TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, level, components, tooltipFlag);
+        components.add(Component.translatable("Hammer breaks: " + (this.radius * 2 + 1) + "x" + (this.radius * 2 + 1)));
+    }
+
+    @Override
+    public void onCraftedBy(@NotNull ItemStack stack, @NotNull Level level, @NotNull Player player) {
+        super.onCraftedBy(stack, level, player);
+        if (infinite) {
+            CompoundTag tag = stack.getOrCreateTag(); // Added Unbreakable tag
+            tag.putBoolean("Unbreakable", true);
+        }
+    }
+
+    private static void blockPos(BlockHitResult hit, List<Direction> dir, List<BlockPos> pos,
+                                 BlockPos block, int rad) {
+        if (hit.getDirection() == dir.get(0) || hit.getDirection() == dir.get(1)) {
+            for (int x = -rad; x <= rad; x++) {
+                for (int y = -rad; y <= rad; y++) {
+                    switch (dir.get(0)) {
+                        case DOWN, UP -> pos.add(new BlockPos(block.getX() + x, block.getY(), block.getZ() + y));
+                        case NORTH, SOUTH -> pos.add(new BlockPos(block.getX() + x, block.getY() + y, block.getZ()));
+                        case EAST, WEST -> pos.add(new BlockPos(block.getX(), block.getY() + y, block.getZ() + x));
+                    }
+                }
+            }
+        }
     }
 
     // Hammer Highlight Block
@@ -102,36 +108,23 @@ public class HammerItem extends DiggerItem implements Vanishable {
     public static void clientTick() {
         if (ticksRemaining > 0) {
             ticksRemaining--;
-            if (ticksRemaining == 0) {
-                highlightedBlocks = List.of(); // Clean Highlighted Blocks list
-            }
+            if (ticksRemaining == 0) { highlightedBlocks = List.of(); } // Clean Highlighted Blocks list
         }
     }
 
     // Render highlight blocks
-    public static void renderHighlight(PoseStack poseStack, Camera camera, MultiBufferSource bufferSource) {
-        if (highlightedBlocks.isEmpty()) { return; }
-        Vec3 camPos = camera.getPosition(); // Player position
-        // Highlighted Blocks Color
-        for (BlockPos pos : highlightedBlocks) {
-            AABB box = new AABB(pos).move(-camPos.x, -camPos.y, -camPos.z);
-            drawBox(poseStack, bufferSource, box, 1f, 1f, 0f, 0.4f); // Transparent Yellow
+    public static void renderHighlight(PoseStack pose, Camera camera, MultiBufferSource buffer) {
+        if (!highlightedBlocks.isEmpty()) {
+            Vec3 camPos = camera.getPosition(); // Player position
+            highlightedBlocks.forEach(pos -> { AABB box = new AABB(pos).move(-camPos.x, -camPos.y, -camPos.z);
+                drawBox(pose, buffer, box, 1f, 1f, 0f, 0.4f); }); // Highlighted blocks transparent Yellow
         }
     }
 
     // Draw Box to highlight blocks
-    public static void drawBox(PoseStack stack, MultiBufferSource buffer, AABB box,
+    public static void drawBox(PoseStack pose, MultiBufferSource buffer, AABB box,
                                float r, float g, float b, float alpha) {
         VertexConsumer builder = buffer.getBuffer(RenderType.lines());
-        LevelRenderer.renderLineBox(stack, builder, box, r, g, b, alpha);
-    }
-
-    @Override
-    public void onCraftedBy(@NotNull ItemStack stack, @NotNull Level level, @NotNull Player player) {
-        super.onCraftedBy(stack, level, player);
-        if (infinite) {
-            CompoundTag tag = stack.getOrCreateTag(); // Added Unbreakable tag
-            tag.putBoolean("Unbreakable", true);
-        }
+        LevelRenderer.renderLineBox(pose, builder, box, r, g, b, alpha);
     }
 }
