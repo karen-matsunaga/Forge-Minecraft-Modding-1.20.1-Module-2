7a8bed732e7e971405dcb48fd7bf646aae19d9c0
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-05-25 10:41:06 -0300

    2025-05-25 Organized Data Tablet comments and code

--- src/main/java/net/karen/mccourse/item/custom/DataTabletItem.java
+++ src/main/java/net/karen/mccourse/item/custom/DataTabletItem.java
@@ -9,37 +9,35 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class DataTabletItem extends Item {
-    public DataTabletItem(Properties pProperties) { super(pProperties); }
+    public DataTabletItem(Properties properties) { super(properties); }
 
     // If player clicking without looking a block
     @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand hand) {
         // Delete the tag in a hand to player
-        if (pPlayer.getItemInHand(pUsedHand).hasTag()) {
-            pPlayer.getItemInHand(pUsedHand).setTag(new CompoundTag());
-        }
-        return super.use(pLevel, pPlayer, pUsedHand);
+        if (player.getItemInHand(hand).hasTag()) { player.getItemInHand(hand).setTag(new CompoundTag()); }
+        return super.use(level, player, hand);
     }
 
     // Enchantment item shine - Saved on particular item
     @Override
-    public boolean isFoil(ItemStack pStack) { return pStack.hasTag(); }
+    public boolean isFoil(ItemStack stack) { return stack.hasTag(); }
 
     // If found some ore automatically return to data with coordinates (X, Y, Z)
     @Override
-    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel,
-                                List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+    public void appendHoverText(ItemStack stack, @Nullable Level level, @NotNull List<Component> components,
+                                @NotNull TooltipFlag tooltipFlag) {
         // If an item is enchantment
-        if(pStack.hasTag()) {
-            String currentFoundOre = pStack.getTag().getString("mccourse.found_ore");
-            // Item received more information with coordinates
-            pTooltipComponents.add(Component.literal(currentFoundOre));
+        if (stack.getTag() != null && stack.hasTag()) {
+            String currentFoundOre = stack.getTag().getString("mccourse.found_ore");
+            components.add(Component.literal(currentFoundOre)); // Item received more information with coordinates
         }
-        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+        super.appendHoverText(stack, level, components, tooltipFlag);
     }
 }
