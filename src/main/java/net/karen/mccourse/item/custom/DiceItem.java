b5da4f7eae72954cff36fded7f79807dc53ad042
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-05-25 15:17:30 -0300

    2025-05-25 Organized Dice comments and code

--- src/main/java/net/karen/mccourse/item/custom/DiceItem.java
+++ src/main/java/net/karen/mccourse/item/custom/DiceItem.java
@@ -10,23 +10,24 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class DiceItem extends Item {
-    public DiceItem(Properties pProperties) { super(pProperties); }
+    public DiceItem(Properties properties) { super(properties); }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        ItemStack itemstack = pPlayer.getItemInHand(pUsedHand);
-        pLevel.playSound((Player)null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(),
-                SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
-        if (!pLevel.isClientSide) {
-            DiceProjectileEntity diceProjectile = new DiceProjectileEntity(pLevel, pPlayer);
+    public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, @NotNull InteractionHand pUsedHand) {
+        ItemStack itemstack = player.getItemInHand(pUsedHand);
+        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOWBALL_THROW,
+                SoundSource.NEUTRAL, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
+        if (!level.isClientSide()) {
+            DiceProjectileEntity diceProjectile = new DiceProjectileEntity(level, player);
             diceProjectile.setItem(itemstack);
-            diceProjectile.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0.0F, 1.5F, 0F);
-            pLevel.addFreshEntity(diceProjectile);
+            diceProjectile.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 0F);
+            level.addFreshEntity(diceProjectile);
         }
-        pPlayer.awardStat(Stats.ITEM_USED.get(this));
-        if (!pPlayer.getAbilities().instabuild) { itemstack.shrink(1); }
-        return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
+        player.awardStat(Stats.ITEM_USED.get(this));
+        if (!player.getAbilities().instabuild) { itemstack.shrink(1); }
+        return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
     } // When used Dice item pressed right mouse button
 }
