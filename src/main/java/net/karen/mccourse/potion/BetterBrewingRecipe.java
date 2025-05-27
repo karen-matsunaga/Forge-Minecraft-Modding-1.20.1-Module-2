6b2e7c63e84b195759445cce1b5d3d0048aed669
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-05-27 10:48:27 -0300

    2025-05-27 Organized Better Brewing Recipe comments and code

--- src/main/java/net/karen/mccourse/potion/BetterBrewingRecipe.java
+++ src/main/java/net/karen/mccourse/potion/BetterBrewingRecipe.java
@@ -8,8 +8,8 @@ import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.common.brewing.IBrewingRecipe;
 import org.jetbrains.annotations.NotNull;
 
-// BetterBrewingRecipe Class by CAS-ual-TY from https://github.com/CAS-ual-TY/Extra-Potions (GPL-3.0 License)
-// https://github.com/CAS-ual-TY/Extra-Potions/blob/main/LICENSE
+/* BetterBrewingRecipe Class by CAS-ual-TY from https://github.com/CAS-ual-TY/Extra-Potions
+   (GPL-3.0 License) - https://github.com/CAS-ual-TY/Extra-Potions/blob/main/LICENSE */
 public class BetterBrewingRecipe implements IBrewingRecipe {
     private final Potion input;
     private final Item ingredient;
@@ -33,7 +33,6 @@ public class BetterBrewingRecipe implements IBrewingRecipe {
     @Override
     public @NotNull ItemStack getOutput(@NotNull ItemStack input, @NotNull ItemStack ingredient) {
         if (!this.isInput(input) || !this.isIngredient(ingredient)) { return ItemStack.EMPTY; }
-
         ItemStack itemStack = new ItemStack(input.getItem());
         itemStack.setTag(new CompoundTag());
         PotionUtils.setPotion(itemStack, this.output);
