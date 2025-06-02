5d3ebd9c81d1b99e4a9cfb26116dbc4f95fe31b1
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-06-02 20:54:19 -0300

    2025-06-02 Improved ModEnchantments comments and code

--- src/main/java/net/karen/mccourse/enchantment/ModEnchantments.java
+++ src/main/java/net/karen/mccourse/enchantment/ModEnchantments.java
@@ -2,12 +2,9 @@ package net.karen.mccourse.enchantment;
 
 import net.karen.mccourse.MCCourseMod;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.*;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.*;
 
 public class ModEnchantments {
     public static final DeferredRegister<Enchantment> ENCHANTMENTS =
@@ -15,63 +12,44 @@ public class ModEnchantments {
 
     // Registry all custom enchantments - Books and enchantment's levels are adding automatically in game
     // Lightning Striker enchantment - SWORD enchantment
-    public static final RegistryObject<Enchantment> LIGHTNING_STRIKER =
-            ENCHANTMENTS.register("lightning_striker",
-                    () -> new LightningStrikerEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.WEAPON,
-                            EquipmentSlot.MAINHAND));
+    public static final RegistryObject<Enchantment> LIGHTNING_STRIKER = ENCHANTMENTS.register("lightning_striker",
+            () -> new LightningStrikerEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.WEAPON, EquipmentSlot.MAINHAND));
 
     // Glowing Mobs enchantment - HELMET enchantment
-    public static final RegistryObject<Enchantment> GLOWING_MOBS =
-            ENCHANTMENTS.register("glowing_mobs",
-                    () -> new GlowingMobsEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.ARMOR_HEAD,
-                            EquipmentSlot.HEAD));
+    public static final RegistryObject<Enchantment> GLOWING_MOBS = ENCHANTMENTS.register("glowing_mobs",
+            () -> new GlowingMobsEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.ARMOR_HEAD, EquipmentSlot.HEAD));
 
-    public static final RegistryObject<Enchantment> GLOWING_BLOCKS =
-            ENCHANTMENTS.register("glowing_blocks",
-                    () -> new GlowingBlocksEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.ARMOR_HEAD,
-                            EquipmentSlot.HEAD));
+    // Glowing Blocks enchantment - HELMET enchantment
+    public static final RegistryObject<Enchantment> GLOWING_BLOCKS = ENCHANTMENTS.register("glowing_blocks",
+            () -> new GlowingBlocksEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.ARMOR_HEAD, EquipmentSlot.HEAD));
 
     // More Ores enchantment - PICKAXE, HAMMER, PAXEL enchantment
-    public static final RegistryObject<Enchantment> MORE_ORES =
-            ENCHANTMENTS.register("more_ores",
-                    () -> new MoreOresEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER,
-                            EquipmentSlot.MAINHAND));
+    public static final RegistryObject<Enchantment> MORE_ORES = ENCHANTMENTS.register("more_ores",
+            () -> new MoreOresEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER, EquipmentSlot.MAINHAND));
 
     // Magnetic enchantment - PICKAXE, HAMMER, PAXEL enchantment
-    public static final RegistryObject<Enchantment> MAGNETIC =
-            ENCHANTMENTS.register("magnetic",
-                    () -> new MagneticEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER,
-                            EquipmentSlot.MAINHAND));
+    public static final RegistryObject<Enchantment> MAGNETIC = ENCHANTMENTS.register("magnetic",
+            () -> new MagneticEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER, EquipmentSlot.MAINHAND));
 
     // Auto Smelt enchantment - PICKAXE, HAMMER, PAXEL enchantment
-    public static final RegistryObject<Enchantment> AUTO_SMELT =
-            ENCHANTMENTS.register("auto_smelt",
-                    () -> new AutoSmeltEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER,
-                            EquipmentSlot.MAINHAND));
+    public static final RegistryObject<Enchantment> AUTO_SMELT = ENCHANTMENTS.register("auto_smelt",
+            () -> new AutoSmeltEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER, EquipmentSlot.MAINHAND));
 
     // Rainbow enchantment - PICKAXE, HAMMER, PAXEL enchantment
-    public static final RegistryObject<Enchantment> RAINBOW =
-            ENCHANTMENTS.register("rainbow",
-                    () -> new RainbowEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER,
-                            EquipmentSlot.MAINHAND));
+    public static final RegistryObject<Enchantment> RAINBOW = ENCHANTMENTS.register("rainbow",
+            () -> new RainbowEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER, EquipmentSlot.MAINHAND));
 
     // Block Fly enchantment - PICKAXE, HAMMER, PAXEL enchantment
-    public static final RegistryObject<Enchantment> BLOCK_FLY =
-            ENCHANTMENTS.register("block_fly",
-                    () -> new BlockFlyEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER,
-                            EquipmentSlot.MAINHAND));
+    public static final RegistryObject<Enchantment> BLOCK_FLY = ENCHANTMENTS.register("block_fly",
+            () -> new BlockFlyEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.DIGGER, EquipmentSlot.MAINHAND));
 
     // Eternal enchantment - Armor and Tools enchantment
-    public static final RegistryObject<Enchantment> ETERNAL =
-            ENCHANTMENTS.register("eternal",
-                    () -> new EternalEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.BREAKABLE,
-                            EquipmentSlot.values()));
+    public static final RegistryObject<Enchantment> ETERNAL = ENCHANTMENTS.register("eternal",
+            () -> new EternalEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.BREAKABLE, EquipmentSlot.values()));
 
     // Recover enchantment - Armor and Tools enchantment
-    public static final RegistryObject<Enchantment> RECOVER =
-            ENCHANTMENTS.register("recover",
-                    () -> new RecoverEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.BREAKABLE,
-                            EquipmentSlot.values()));
+    public static final RegistryObject<Enchantment> RECOVER = ENCHANTMENTS.register("recover",
+            () -> new RecoverEnchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.BREAKABLE, EquipmentSlot.values()));
 
     // Registry all enchantments in game
     public static void register(IEventBus eventBus) { ENCHANTMENTS.register(eventBus); }
