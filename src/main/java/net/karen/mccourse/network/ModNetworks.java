0848e3ca248a07b0d3366f74a80348025a48acf4
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-06-03 21:07:38 -0300

    2025-06-03 Improved ModNetworks comments and code

--- src/main/java/net/karen/mccourse/network/ModNetworks.java
+++ src/main/java/net/karen/mccourse/network/ModNetworks.java
@@ -2,13 +2,9 @@ package net.karen.mccourse.network;
 
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.*;
 import net.minecraftforge.network.simple.SimpleChannel;
-
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class ModNetworks {
     // START and END of user code block mod methods
