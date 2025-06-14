00a601fbcc113abe0d5df1f84b68c5723fff2d1f
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-06-14 16:04:38 -0300

    2025-06-14 Added Mccourse Hammer distance parameter, updated item description and improved comments

--- src/main/java/net/karen/mccourse/item/custom/MccourseHammerItem.java
+++ src/main/java/net/karen/mccourse/item/custom/MccourseHammerItem.java
@@ -12,42 +12,47 @@ import org.jetbrains.annotations.*;
 import java.util.List;
 
 public class MccourseHammerItem extends DiggerItem implements Vanishable {
+    private final int distance; // 2 x 1 x Z -> Break straight distance
+
     public MccourseHammerItem(Tier tier, float attackDamageModifier, float attackSpeedModifier,
-                              TagKey<Block> blockTags, Properties properties) {
+                              Properties properties, TagKey<Block> blockTags, int distance) {
         super(attackDamageModifier, attackSpeedModifier, tier, blockTags, properties);
+        this.distance = distance;
     }
 
     @Override
     public boolean mineBlock(@NotNull ItemStack stack, Level level, @NotNull BlockState state,
                              @NotNull BlockPos pos, @NotNull LivingEntity entity) {
-        if (!level.isClientSide()) { hammerMode(level, pos, entity); }
+        if (!level.isClientSide()) { hammer(level, pos, entity); }
         return super.mineBlock(stack, level, state, pos, entity);
     }
 
-    public void hammerMode(LevelAccessor world, BlockPos origin, Entity entity) {
-        Direction direction = getFacingDirection(entity);
-        switch (direction) { // If looking UP or DOWN: break 2 vertical blocks (y and y+1)
-            case UP, DOWN -> { blocks(origin.above(), world); blocks(origin.below(), world); }
-            // If looking NORTH, SOUTH, WEST or EAST: break 2 vertical blocks (y and y-1)
-            case NORTH, SOUTH, WEST, EAST -> blocks(origin.below(), world);
+    // CUSTOM METHOD - Hammer Mode
+    public void hammer(LevelAccessor world, BlockPos origin, Entity entity) {
+        double x = entity.getLookAngle().x, y = entity.getLookAngle().y, z = entity.getLookAngle().z;
+        Direction direction = Direction.getNearest(x, y, z);
+        BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
+        for (int i = 0; i < distance; i++) {
+            pos.set(origin).move(direction, i); // Calculate the position of the block to be broken at step i in the look direction
+            blocks(pos, world); // Breaks the block at the current height (y)
+            pos.setY(pos.getY() + 1); // Break the block above (y + 1) to make height 2
+            blocks(pos, world);
         }
-    } // CUSTOM METHOD - Hammer breaks 2x1
+    }
 
-    private Direction getFacingDirection(Entity entity) {
-        return Direction.getNearest(entity.getLookAngle().x, entity.getLookAngle().y, entity.getLookAngle().z);
-    } // CUSTOM METHOD - Gets the direction the player is looking
-
+    // CUSTOM METHOD - Break the block in the world
     private void blocks(BlockPos position, LevelAccessor world) {
-        if (!world.isEmptyBlock(position)) { // CUSTOM METHOD - Break the block in the world
+        if (!world.isEmptyBlock(position)) {
             Block.dropResources(world.getBlockState(position), world, position, null);
             world.destroyBlock(position, false);
         }
     }
 
+    // Appears TOOLTIP on screen of Hammer, Paxel, etc.
     @Override
     public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level,
                                 @NotNull List<Component> components, @NotNull TooltipFlag tooltipFlag) {
-        components.add(Component.literal("Hammer breaks: 2x1")); // Appears TOOLTIP on screen of Hammer, Paxel, etc.
+        components.add(Component.literal("Hammer breaks: 2x1x" + this.distance));
         super.appendHoverText(stack, level, components, tooltipFlag);
     }
 }
