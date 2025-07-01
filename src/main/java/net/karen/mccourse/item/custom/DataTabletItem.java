2c14dc8dee5927f50cda0c074e8711bac6edcfb9
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-01 16:16:09 -0300

    2025-07-01 Improved Data Tablet item comments and codes

--- src/main/java/net/karen/mccourse/item/custom/DataTabletItem.java
+++ src/main/java/net/karen/mccourse/item/custom/DataTabletItem.java
@@ -1,5 +1,7 @@
 package net.karen.mccourse.item.custom;
 
+import net.karen.mccourse.util.ChatUtil;
+import net.karen.mccourse.util.Utils;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
@@ -11,33 +13,29 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
 import java.util.List;
 
 public class DataTabletItem extends Item {
     public DataTabletItem(Properties properties) { super(properties); }
 
-    // If player clicking without looking a block
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand hand) {
-        // Delete the tag in a hand to player
-        if (player.getItemInHand(hand).hasTag()) { player.getItemInHand(hand).setTag(new CompoundTag()); }
+        ItemStack mainHand = player.getItemInHand(hand); // If player clicking without looking a block delete the tag in hand
+        if (mainHand.hasTag()) { mainHand.setTag(new CompoundTag()); }
         return super.use(level, player, hand);
     }
 
-    // Enchantment item shine - Saved on particular item
     @Override
-    public boolean isFoil(ItemStack stack) { return stack.hasTag(); }
+    public boolean isFoil(ItemStack stack) { return stack.hasTag(); } // Enchantment item shine - Saved on particular item
 
-    // If found some ore automatically return to data with coordinates (X, Y, Z)
     @Override
-    public void appendHoverText(ItemStack stack, @Nullable Level level, @NotNull List<Component> components,
-                                @NotNull TooltipFlag tooltipFlag) {
-        // If an item is enchantment
-        if (stack.getTag() != null && stack.hasTag()) {
+    public void appendHoverText(ItemStack stack, @Nullable Level level,
+                                @NotNull List<Component> list, @NotNull TooltipFlag flag) {
+        // If found some ore automatically return to data with coordinates (X, Y, Z)
+        if (stack.getTag() != null && stack.hasTag()) { // If an item is enchantment
             String currentFoundOre = stack.getTag().getString("mccourse.found_ore");
-            components.add(Component.literal(currentFoundOre)); // Item received more information with coordinates
+            ChatUtil.tooltipLine(list, currentFoundOre, Utils.gold); // Item received more information with coordinates
         }
-        super.appendHoverText(stack, level, components, tooltipFlag);
+        super.appendHoverText(stack, level, list, flag);
     }
 }
