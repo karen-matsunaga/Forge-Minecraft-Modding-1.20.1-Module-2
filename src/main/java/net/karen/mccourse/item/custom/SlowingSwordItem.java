6ca4c5f2ca54afda3f472a5910120b42031bd226
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-01 16:19:47 -0300

    2025-07-01 Improved Slowing sword item comments and codes

--- src/main/java/net/karen/mccourse/item/custom/SlowingSwordItem.java
+++ src/main/java/net/karen/mccourse/item/custom/SlowingSwordItem.java
@@ -10,15 +10,14 @@ import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 
 public class SlowingSwordItem extends SwordItem {
-    public SlowingSwordItem(Tier tier, int pAttackDamageModifier,
-                            float pAttackSpeedModifier, Properties properties) {
-        super(tier, pAttackDamageModifier, pAttackSpeedModifier, properties);
+    public SlowingSwordItem(Tier tier, int attackDamageModifier,
+                            float attackSpeedModifier, Properties properties) {
+        super(tier, attackDamageModifier, attackSpeedModifier, properties);
     }
 
-    // Every time the player hit on entity to create slowing effect only to a living entity
     @Override
     public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
-        if (entity instanceof LivingEntity livingEntity) {
+        if (entity instanceof LivingEntity livingEntity) { // Every time the player hit on entity to create slowing effect
             livingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 400), player);
         }
         return super.onLeftClickEntity(stack, player, entity);
