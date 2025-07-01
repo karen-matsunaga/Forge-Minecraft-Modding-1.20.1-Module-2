875a6d8a9bbcf4fb67eb01f1b699ce22fe2864ba
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-01 16:14:12 -0300

    2025-07-01 Improved Bouncy Balls item comments and codes

--- src/main/java/net/karen/mccourse/entity/custom/BouncyBallsProjectileEntity.java
+++ src/main/java/net/karen/mccourse/entity/custom/BouncyBallsProjectileEntity.java
@@ -1,8 +1,10 @@
 package net.karen.mccourse.entity.custom;
 
+import net.karen.mccourse.item.ModItems;
 import net.karen.mccourse.particle.ModParticles;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -13,69 +15,67 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.common.util.ITeleporter;
-import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.event.entity.EntityTeleportEvent.EnderPearl;
-
+import org.jetbrains.annotations.NotNull;
 import javax.annotation.Nullable;
+import static net.minecraftforge.event.ForgeEventFactory.onEnderPearlLand;
 
 public class BouncyBallsProjectileEntity extends ThrownEnderpearl {
-    public BouncyBallsProjectileEntity(EntityType<? extends ThrownEnderpearl> pEntityType, Level pLevel) { super(pEntityType, pLevel); }
+    private final Entity owner = this.getOwner();
 
-    public BouncyBallsProjectileEntity(Level pLevel, LivingEntity pShooter) { super(pLevel, pShooter); }
-
-    protected Item getDefaultItem() { return null; }
-
-    // Called when the arrow hits an entity
-    protected void onHitEntity(EntityHitResult pResult) {
-        super.onHitEntity(pResult);
-        pResult.getEntity().hurt(this.damageSources().thrown(this, this.getOwner()), 0.0F);
+    public BouncyBallsProjectileEntity(EntityType<? extends ThrownEnderpearl> entityType, Level level) {
+        super(entityType, level);
     }
 
-    // Called when this EntityFireball hits a block or entity.
-    protected void onHit(HitResult pResult) {
-        super.onHit(pResult);
+    public BouncyBallsProjectileEntity(Level level, LivingEntity shooter) { super(level, shooter); }
 
-        // Particles when used Bouncy Balls item
-        for(int i = 0; i < 32; ++i) {
-            this.level().addParticle(ModParticles.BOUNCY_BALLS_PARTICLES.get(),
-                    this.getX(), this.getY() + this.random.nextDouble() * 2.0D, this.getZ(),
-                    this.random.nextGaussian(), 0.0D, this.random.nextGaussian());
+    // CUSTOM METHOD - Default item
+    protected @NotNull Item getDefaultItem() { return ModItems.BOUNCY_BALLS.get(); }
+
+    // CUSTOM METHOD - Called when the arrow hits an entity
+    protected void onHitEntity(@NotNull EntityHitResult result) {
+        super.onHitEntity(result);
+        result.getEntity().hurt(this.damageSources().thrown(this, owner), 0.0F);
+    }
+
+    // CUSTOM METHOD - Called when this EntityFireball hits a block or entity.
+    protected void onHit(@NotNull HitResult result) {
+        super.onHit(result);
+        RandomSource source = this.random;
+        double x = this.getX(), y = this.getY(), z = this.getZ(), gaus = source.nextGaussian(), dou = source.nextDouble();
+        for (int i = 0; i < 32; ++i) { // Particles when used Bouncy Balls item
+            this.level().addParticle(ModParticles.BOUNCY_BALLS_PARTICLES.get(), x, y + dou * 2.0D, z, gaus, 0.0D, gaus);
         }
-
         if (!this.level().isClientSide && !this.isRemoved()) {
-            Entity entity = this.getOwner();
-            if (entity instanceof ServerPlayer) {
-                ServerPlayer serverplayer = (ServerPlayer)entity;
-                if (serverplayer.connection.isAcceptingMessages() && serverplayer.level() == this.level() && !serverplayer.isSleeping()) {
-                    EnderPearl event = ForgeEventFactory.onEnderPearlLand(serverplayer, this.getX(), this.getY(), this.getZ(), this, 5.0F, pResult);
-
-                    if (!event.isCanceled()) {
-                        // Don't indent to lower patch size
-                        if (entity.isPassenger()) { serverplayer.dismountTo(this.getX(), this.getY(), this.getZ()); }
-                        else { entity.teleportTo(this.getX(), this.getY(), this.getZ()); }
-
-                        entity.teleportTo(event.getTargetX(), event.getTargetY(), event.getTargetZ());
-                        entity.resetFallDistance();
-                        entity.hurt(this.damageSources().fall(), event.getAttackDamage());
+            if (owner instanceof ServerPlayer server) {
+                if (server.connection.isAcceptingMessages() && server.level() == this.level() && !server.isSleeping()) {
+                    EnderPearl event = onEnderPearlLand(server, x, y, z, this, 5.0F, result);
+                    if (!event.isCanceled()) { // Don't indent to lower patch size
+                        if (owner.isPassenger()) { server.dismountTo(x, y, z); }
+                        else { owner.teleportTo(x, y, z); }
+                        owner.teleportTo(event.getTargetX(), event.getTargetY(), event.getTargetZ());
+                        owner.resetFallDistance();
+                        owner.hurt(this.damageSources().fall(), event.getAttackDamage());
                     } // Forge: End
                 }
             }
-            else if (entity != null) { entity.teleportTo(this.getX(), this.getY(), this.getZ()); entity.resetFallDistance(); }
+            else if (owner != null) { // Player teleport
+                owner.teleportTo(x, y, z);
+                owner.resetFallDistance();
+            }
             this.discard();
         }
     }
 
-    // Called to update the entity's position/logic.
+    // CUSTOM METHOD - Called to update the entity's position/logic
     public void tick() {
-        Entity entity = this.getOwner();
-        if (entity instanceof Player && !entity.isAlive()) { this.discard(); }
+        if (owner instanceof Player && !owner.isAlive()) { this.discard(); }
         else { super.tick(); }
     }
 
     @Nullable
-    public Entity changeDimension(ServerLevel pServer, ITeleporter teleporter) {
-        Entity entity = this.getOwner();
-        if (entity != null && entity.level().dimension() != pServer.dimension()) { this.setOwner(null); }
-        return super.changeDimension(pServer, teleporter);
+    public Entity changeDimension(@NotNull ServerLevel level, @NotNull ITeleporter teleporter) {
+        if (owner != null && owner.level().dimension() != level.dimension()) { this.setOwner(null); }
+        return super.changeDimension(level, teleporter);
     }
 }
--- src/main/java/net/karen/mccourse/item/custom/BouncyBallsItem.java
+++ src/main/java/net/karen/mccourse/item/custom/BouncyBallsItem.java
@@ -2,8 +2,8 @@ package net.karen.mccourse.item.custom;
 
 import net.karen.mccourse.entity.custom.BouncyBallsProjectileEntity;
 import net.karen.mccourse.item.ModItems;
+import net.karen.mccourse.util.Utils;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -16,22 +16,21 @@ import org.jetbrains.annotations.NotNull;
 public class BouncyBallsItem extends Item {
     public BouncyBallsItem(Properties properties) { super(properties); }
 
-    public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player,
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player,
                                                            @NotNull InteractionHand hand) {
-        ItemStack itemstack = player.getItemInHand(hand);
-        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENDER_PEARL_THROW,
-                SoundSource.NEUTRAL, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
+        ItemStack itemstack = player.getItemInHand(hand); // Player has Bouncy Balls item on hand
+        float volume = 0.5F, pitch = 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F);
+        Utils.neutralSound(level, player, SoundEvents.ENDER_PEARL_THROW, volume, pitch); // Sound when used item
         player.getCooldowns().addCooldown(this, 0); // Nothing cooldown
         if (!level.isClientSide()) {
-            BouncyBallsProjectileEntity thrownbouncyballs = new BouncyBallsProjectileEntity(level, player);
-            thrownbouncyballs.setItem(new ItemStack(ModItems.BOUNCY_BALLS_PARTICLES.get()));
-            thrownbouncyballs.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
-            level.addFreshEntity(thrownbouncyballs);
+            float x = player.getXRot(), y = player.getYRot(), z = 0.0F, velocity = 1.5F, inaccuracy = 1.0F;
+            BouncyBallsProjectileEntity thrownBouncyBalls = new BouncyBallsProjectileEntity(level, player);
+            thrownBouncyBalls.setItem(new ItemStack(ModItems.BOUNCY_BALLS_PARTICLES.get()));
+            thrownBouncyBalls.shootFromRotation(player, x, y, z, velocity, inaccuracy);
+            level.addFreshEntity(thrownBouncyBalls); // When hit appears Bouncy Balls particles
         }
-
         player.awardStat(Stats.ITEM_USED.get(this));
         if (!player.getAbilities().instabuild) { itemstack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(hand)); }
-
         return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
     }
 }
