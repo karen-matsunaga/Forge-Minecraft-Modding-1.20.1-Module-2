0c67d1dcd72198046d9caf6b5763193b723cde85
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-03 22:49:31 -0300

    2025-07-03 Improved Dice item comments and codes

--- src/main/java/net/karen/mccourse/item/custom/DiceItem.java
+++ src/main/java/net/karen/mccourse/item/custom/DiceItem.java
@@ -1,25 +1,22 @@
 package net.karen.mccourse.item.custom;
 
 import net.karen.mccourse.entity.custom.DiceProjectileEntity;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.*;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+import static net.karen.mccourse.util.Utils.*;
+import static net.minecraft.sounds.SoundEvents.*;
 
 public class DiceItem extends Item {
     public DiceItem(Properties properties) { super(properties); }
 
     @Override
-    public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, @NotNull InteractionHand pUsedHand) {
-        ItemStack itemstack = player.getItemInHand(pUsedHand);
-        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOWBALL_THROW,
-                SoundSource.NEUTRAL, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand); // When used Dice item pressed right mouse button
+        neutralSound(level, player, SNOWBALL_THROW, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
         if (!level.isClientSide()) {
             DiceProjectileEntity diceProjectile = new DiceProjectileEntity(level, player);
             diceProjectile.setItem(itemstack);
@@ -29,5 +26,5 @@ public class DiceItem extends Item {
         player.awardStat(Stats.ITEM_USED.get(this));
         if (!player.getAbilities().instabuild) { itemstack.shrink(1); }
         return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
-    } // When used Dice item pressed right mouse button
+    }
 }
