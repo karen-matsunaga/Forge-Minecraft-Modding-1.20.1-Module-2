9bb6aa1297fd97883e7add2c4bfc997108a80939
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-04 11:04:52 -0300

    2025-07-04 Improved Destroyer item comments and codes

--- src/main/java/net/karen/mccourse/item/custom/DestroyerItem.java
+++ src/main/java/net/karen/mccourse/item/custom/DestroyerItem.java
@@ -1,60 +1,50 @@
 package net.karen.mccourse.item.custom;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.NonNullList;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.*;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
-
 import java.util.List;
+import static net.karen.mccourse.util.ChatUtil.*;
+import static net.karen.mccourse.util.Utils.*;
 
 public class DestroyerItem extends Item {
     public DestroyerItem(Properties properties) { super(properties); }
 
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(Level level, @NotNull Player player, @NotNull InteractionHand hand) {
-        // Player has Destroyer item on Main hand
-        if (level.isClientSide()) { return InteractionResultHolder.pass(player.getItemInHand(hand)); }
-
-        // Block below the player
-        BlockState state = level.getBlockState(player.blockPosition().below());
-
-        // Items received according to the block clicked
-        ItemStack targetItem = new ItemStack(state.getBlock().asItem());
-
-        // All recipes from Crafting Recipe
-        List<Recipe<?>> matchingRecipes = level.getRecipeManager().getRecipes().stream()
-                .filter(recipe -> recipe.getResultItem(level.registryAccess()).getItem() == targetItem.getItem()).toList();
-
-        // If not has recipe display on screen message
-        if (matchingRecipes.isEmpty()) {
-            player.displayClientMessage(Component.literal("No recipes found for this item."), true);
-            return InteractionResultHolder.success(player.getItemInHand(hand));
-        }
-
-        // For each recipe found
-        for (Recipe<?> recipe : matchingRecipes) {
-            NonNullList<Ingredient> ingredients = recipe.getIngredients();
-
-            // Drop all input ingredients also variants to Player's inventory
-            for (Ingredient ingredient : ingredients) {
-                for (ItemStack possibleMatch : ingredient.getItems()) {
-                    ItemStack stackToGive = possibleMatch.copy();
-                    player.getInventory().add(stackToGive);
+        ItemStack item = player.getItemInHand(hand); // Player has Destroyer item on Main hand
+        if (!level.isClientSide()) {
+            BlockState state = level.getBlockState(player.blockPosition().below()); // Block below the player
+            ItemStack targetItem = new ItemStack(state.getBlock().asItem()); // Items received according to the block clicked
+            List<Recipe<?>> matchingRecipes = level.getRecipeManager().getRecipes().stream() // All recipes from Crafting Recipe
+                            .filter(recipe -> recipe.getResultItem(level.registryAccess()).getItem() == targetItem.getItem()).toList();
+            // If not has recipe display on screen message
+            if (matchingRecipes.isEmpty()) { return messages(player, "No recipes found for this item.", red, item); }
+            for (Recipe<?> recipe : matchingRecipes) { // For each recipe found
+                NonNullList<Ingredient> ingredients = recipe.getIngredients();
+                for (Ingredient ingredient : ingredients) {
+                    for (ItemStack possibleMatch : ingredient.getItems()) {
+                        ItemStack stackToGive = possibleMatch.copy();
+                        player.getInventory().add(stackToGive); // Drop all input ingredients also variants to Player's inventory
+                    }
                 }
             }
+            // Display on screen message when has recipe and Player received items on inventory
+            return messages(player, "Ingredients returned from " + matchingRecipes.size() + " recipes.", green, item);
         }
+        return InteractionResultHolder.pass(item);
+    }
 
-        // Display on screen message when has recipe and Player received items on inventory
-        player.displayClientMessage(Component.literal("Ingredients returned from " +
-                matchingRecipes.size() + " recipes."), true);
-        return InteractionResultHolder.success(player.getItemInHand(hand));
+    // CUSTOM METHOD - Success messages
+    private InteractionResultHolder<ItemStack> messages(Player player, String message,
+                                                        ChatFormatting color, ItemStack item) {
+        player(player, message, color);
+        return InteractionResultHolder.success(item);
     }
 }
