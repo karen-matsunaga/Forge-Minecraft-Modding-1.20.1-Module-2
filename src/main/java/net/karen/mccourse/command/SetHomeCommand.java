af1b86b253e3e976bfe80aadf5ccb9a6f54ad925
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-06 22:01:47 -0300

    2025-07-06 Added prevention of repeating home name in Set Home command

--- src/main/java/net/karen/mccourse/command/SetHomeCommand.java
+++ src/main/java/net/karen/mccourse/command/SetHomeCommand.java
@@ -3,11 +3,12 @@ package net.karen.mccourse.command;
 import com.mojang.brigadier.*;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import net.karen.mccourse.util.Utils;
 import net.minecraft.commands.*;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
 import static net.karen.mccourse.util.ChatUtil.*;
+import static net.karen.mccourse.util.Utils.*;
 
 public class SetHomeCommand {
     public SetHomeCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -20,13 +21,20 @@ public class SetHomeCommand {
         ServerPlayer player = context.getSource().getPlayer();
         String name = StringArgumentType.getString(context, "name");
         if (player != null) {
-            BlockPos pos = player.blockPosition(); // Player's [X, Y, Z] block positions
             PlayerHomesData data = PlayerHomesData.get(player.serverLevel());
+            CompoundTag homes = data.getHomes(player.getUUID());
+            if (homes.contains(name)) {
+                String hasHome = "A home named §6§l" + name + "§4§l already exists!";
+                context.getSource().sendFailure(componentLiteralStyle(hasHome, darkRed));
+                return -1; // Player's fail position save
+            }
+            BlockPos pos = player.blockPosition(); // Player's [X, Y, Z] block positions
             data.setHome(player.getUUID(), name, pos); // Save data of Player's position
             // Display the message in the chat
             String message = "Set home at §e§l[X: " + pos.getX() + ", Y: " + pos.getY() + ", Z: " + pos.getZ() + "]";
-            context.getSource().sendSuccess(() -> componentLiteralStyle(message, Utils.purple), false);
+            context.getSource().sendSuccess(() -> componentLiteralStyle(message, purple), false);
+            return 1; // Player's position save with success
         }
-        return 1; // Player's position save with success
+        return 0;
     }
 }
