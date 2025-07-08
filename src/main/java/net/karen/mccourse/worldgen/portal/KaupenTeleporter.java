a8114dc5424c35b1f58ad95a2b70635a309c228e
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-08 12:43:49 -0300

    2025-07-08 Improved Kaupen Teleporter comments and codes

--- src/main/java/net/karen/mccourse/worldgen/portal/KaupenTeleporter.java
+++ src/main/java/net/karen/mccourse/worldgen/portal/KaupenTeleporter.java
@@ -6,47 +6,43 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.common.util.ITeleporter;
-
 import java.util.function.Function;
 
 public class KaupenTeleporter implements ITeleporter {
     public static BlockPos thisPos = BlockPos.ZERO;
     public static boolean insideDimension = true;
 
-    public KaupenTeleporter(BlockPos pos, boolean insideDim) { thisPos = pos; insideDimension = insideDim; }
+    public KaupenTeleporter(BlockPos pos, boolean insideDim) {
+        thisPos = pos; insideDimension = insideDim;
+    }
 
     @Override
-    public Entity placeEntity(Entity entity, ServerLevel currentWorld, ServerLevel destinationWorld,
-                              float yaw, Function<Boolean, Entity> repositionEntity) {
-        entity = repositionEntity.apply(false);
-        int y = 61;
-
-        if (!insideDimension) { y = thisPos.getY(); }
-
-        BlockPos destinationPos = new BlockPos(thisPos.getX(), y, thisPos.getZ());
-
-        int tries = 0;
-        while ((destinationWorld.getBlockState(destinationPos).getBlock() != Blocks.AIR) &&
-                !destinationWorld.getBlockState(destinationPos).canBeReplaced(Fluids.WATER) &&
-                (destinationWorld.getBlockState(destinationPos.above()).getBlock()  != Blocks.AIR) &&
-                !destinationWorld.getBlockState(destinationPos.above()).canBeReplaced(Fluids.WATER) && (tries < 25)) {
-            destinationPos = destinationPos.above(2);
-            tries++;
-        }
-
-        entity.setPos(destinationPos.getX(), destinationPos.getY(), destinationPos.getZ());
-
+    public Entity placeEntity(Entity entity, ServerLevel cWorld, ServerLevel dWorld, float yaw, Function<Boolean, Entity> posEntity) {
+        entity = posEntity.apply(false); // REPOSITION entity
+        int posY = 61;
+        if (!insideDimension) { posY = thisPos.getY(); }
+        BlockPos pos = new BlockPos(thisPos.getX(), posY, thisPos.getZ()); // Destination POSITION
+        int tries = 0, x = pos.getX(), y = pos.getY(), z = pos.getZ(); // WHILE loop
+        // Destination WORLD
+        while (isBlock(dWorld, pos) && isBlock(dWorld, pos.above()) && (tries < 25)) { pos = pos.above(2); tries++; }
+        entity.setPos(x, y, z);
         if (insideDimension) {
             boolean doSetBlock = true;
-            for (BlockPos checkPos : BlockPos.betweenClosed(destinationPos.below(10).west(10),
-                    destinationPos.above(10).east(10))) {
-                if (destinationWorld.getBlockState(checkPos).getBlock() instanceof KaupenPortalBlock) { doSetBlock = false; break; }
+            BlockPos below = pos.below(10).west(10), above = pos.above(10).east(10);
+            for (BlockPos checkPos : BlockPos.betweenClosed(below, above)) {
+                if (dWorld.getBlockState(checkPos).getBlock() instanceof KaupenPortalBlock) { doSetBlock = false; break; }
             }
-
-            if (doSetBlock) { destinationWorld.setBlock(destinationPos, ModBlocks.KAUPEN_PORTAL.get().defaultBlockState(), 3); }
+            if (doSetBlock) { dWorld.setBlock(pos, ModBlocks.KAUPEN_PORTAL.get().defaultBlockState(), 3); }
         }
         return entity;
+    }
+
+    // CUSTOM METHOD - Detected block
+    private boolean isBlock(ServerLevel serverLevel, BlockPos position) {
+        BlockState block = serverLevel.getBlockState(position);
+        return (block.getBlock() != Blocks.AIR) && !block.canBeReplaced(Fluids.WATER);
     }
 }
