5ac494fd8c9ec3e05ceea1d6703e717a6f749519
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-11 16:18:47 -0300

    2025-07-11 Improved Mod Food Properties comments and codes

--- src/main/java/net/karen/mccourse/item/ModFoodProperties.java
+++ src/main/java/net/karen/mccourse/item/ModFoodProperties.java
@@ -1,11 +1,23 @@
 package net.karen.mccourse.item;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.*;
 import net.minecraft.world.food.FoodProperties;
+import static net.karen.mccourse.util.Utils.effect;
 
 public class ModFoodProperties {
     // Registry all custom foods
-    public static final FoodProperties KOHLRABI = new FoodProperties.Builder().nutrition(3).saturationMod(0.25f)
-            .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.1f).build();
+    public static final FoodProperties KOHLRABI = registerFoodEffect(3, 0.25F, MobEffects.MOVEMENT_SPEED,
+                                                                     200, 0, 0.1F);
+
+    // CUSTOM METHOD - Registry all custom foods WITH effect
+    private static FoodProperties registerFoodEffect(int nutrition, float saturation,
+                                                     MobEffect mobEffect, int duration, int amplifier, float probability) {
+        return new FoodProperties.Builder().nutrition(nutrition).saturationMod(saturation)
+                   .effect(() -> effect(mobEffect, duration, amplifier), probability).build();
+    }
+
+    // CUSTOM METHOD - Registry all custom foods WITHOUT effect
+    private static FoodProperties registerFood(int nutrition, float saturation) {
+        return new FoodProperties.Builder().nutrition(nutrition).saturationMod(saturation).build();
+    }
 }
