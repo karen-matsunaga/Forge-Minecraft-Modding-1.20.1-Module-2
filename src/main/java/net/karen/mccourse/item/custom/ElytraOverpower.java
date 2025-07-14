49d40e13bfeed448802cf63c10bda33ccfc85c77
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-14 17:05:34 -0300

    2025-07-14 Improved Elytra Overpower comments and codes

--- src/main/java/net/karen/mccourse/item/custom/ElytraOverpower.java
+++ src/main/java/net/karen/mccourse/item/custom/ElytraOverpower.java
@@ -1,31 +1,43 @@
 package net.karen.mccourse.item.custom;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
+import net.karen.mccourse.util.Utils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ElytraItem;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import java.util.List;
+import static net.karen.mccourse.util.ChatUtil.*;
 
 public class ElytraOverpower extends ElytraItem {
+    private static final int[] COLORS = { 0xff5555, 0xffaa00, 0xffff55, 0x55ff55, 0x55ffff, 0x5555ff, 0xff55ff };
+
     public ElytraOverpower(Properties properties) {
         super(properties);
         DispenserBlock.registerBehavior(this, ArmorItem.DISPENSE_ITEM_BEHAVIOR);
     }
 
     @Override
-    public void inventoryTick(@NotNull ItemStack stack, @NotNull Level level,
-                              @NotNull Entity entity, int slotId, boolean isSelected) {
-        Player player = (Player) entity;
+    public void onArmorTick(ItemStack stack, Level level, Player player) {
         MobEffectInstance effect = new MobEffectInstance(MobEffects.REGENERATION, 200, 4,
-                                true, true, true);
+                                                true, true, true);
         boolean isElytra = player.getInventory().getArmor(2).is(this);
         if (isElytra) { player.addEffect(effect); } // Effect Activated
         if (!isElytra) { player.removeEffect(effect.getEffect()); } // Effect Disabled
-        super.inventoryTick(stack, level, entity, slotId, isSelected);
+        super.onArmorTick(stack, level, player);
+    }
+
+    @Override
+    public @NotNull Component getName(ItemStack stack) { return componentTranslatable(stack.getDescriptionId(), Utils.aqua); }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level,
+                                @NotNull List<Component> list, @NotNull TooltipFlag flag) {
+        String message = " with more durability and receive an effect! \nEffect: REGENERATION V.";
+        tooltipLineLiteralRGB(list, COLORS, stack, message);
+        super.appendHoverText(stack, level, list, flag);
     }
 }
