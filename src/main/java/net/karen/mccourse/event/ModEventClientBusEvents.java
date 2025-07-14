8623facc9f0e2709f98a9bf204783a69f60998fd
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-14 17:07:58 -0300

    2025-07-14 Improved Mod Event Client Bus Events comments and codes

--- src/main/java/net/karen/mccourse/event/ModEventClientBusEvents.java
+++ src/main/java/net/karen/mccourse/event/ModEventClientBusEvents.java
@@ -5,13 +5,10 @@ import net.karen.mccourse.block.ModBlocks;
 import net.karen.mccourse.block.entity.ModBlockEntities;
 import net.karen.mccourse.block.entity.renderer.GemEmpoweringBlockEntityRenderer;
 import net.karen.mccourse.util.ImageTooltipComponent;
-import net.karen.mccourse.particle.AlexandriteParticles;
-import net.karen.mccourse.particle.BouncyBallsParticles;
-import net.karen.mccourse.particle.ModParticles;
+import net.karen.mccourse.particle.*;
 import net.karen.mccourse.util.KeyBinding;
 import net.minecraft.client.renderer.BiomeColors;
-import net.minecraft.client.renderer.blockentity.HangingSignRenderer;
-import net.minecraft.client.renderer.blockentity.SignRenderer;
+import net.minecraft.client.renderer.blockentity.*;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.FoliageColor;
 import net.minecraft.world.level.block.state.BlockState;
@@ -24,42 +21,44 @@ import java.util.function.Function;
 @Mod.EventBusSubscriber(modid = MCCourseMod.MOD_ID,
                         bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModEventClientBusEvents {
+    // CUSTOM EVENT - Register custom particles event
     @SubscribeEvent
-    public static void registerParticleFactories(RegisterParticleProvidersEvent event) { // Register custom particles event
-        event.registerSpriteSet(ModParticles.ALEXANDRITE_PARTICLES.get(), AlexandriteParticles.Provider::new);
+    public static void registerParticleFactories(RegisterParticleProvidersEvent event) {
+        event.registerSpriteSet(ModParticles.ALEXANDRITE_PARTICLES.get(), AlexandriteParticles.Provider::new); // Alexandrite
         event.registerSpriteSet(ModParticles.BOUNCY_BALLS_PARTICLES.get(), BouncyBallsParticles.Provider::new); // Bouncy balls
     }
 
+    // CUSTOM EVENT - Register custom block entity renderer
     @SubscribeEvent
-    public static void registerBER(EntityRenderersEvent.RegisterRenderers event) { // Register custom BER
-        // Register custom block entity renderer
+    public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
+        // Register custom BER (Block Entity Renderer)
         event.registerBlockEntityRenderer(ModBlockEntities.GEM_EMPOWERING_STATION_BE.get(),
-                GemEmpoweringBlockEntityRenderer::new);
-
+                                          GemEmpoweringBlockEntityRenderer::new); // Gem Empowering Station block entity
         // Register custom sign
-        event.registerBlockEntityRenderer(ModBlockEntities.MOD_SIGN.get(), SignRenderer::new);
-        event.registerBlockEntityRenderer(ModBlockEntities.MOD_HANGING_SIGN.get(), HangingSignRenderer::new);
+        event.registerBlockEntityRenderer(ModBlockEntities.MOD_SIGN.get(), SignRenderer::new); // Sign
+        event.registerBlockEntityRenderer(ModBlockEntities.MOD_HANGING_SIGN.get(), HangingSignRenderer::new); // Hanging Sing
     }
 
-    // Register custom colored blocks
+    // CUSTOM EVENT - Register custom colored blocks
     @SubscribeEvent
     public static void registerColoredBlocks(RegisterColorHandlersEvent.Block event) {
-        event.register((pState, pLevel, pPos, pTintIndex) -> pLevel != null && pPos != null
-                ? BiomeColors.getAverageFoliageColor(pLevel, pPos)
-                : FoliageColor.getDefaultColor(), ModBlocks.COLORED_LEAVES.get());
+        event.register((state, level, pos, tintIndex) -> level != null && pos != null
+                        ? BiomeColors.getAverageFoliageColor(level, pos)
+                        : FoliageColor.getDefaultColor(), ModBlocks.COLORED_LEAVES.get());
     }
 
+    // CUSTOM EVENT - Register custom colored blocks
     @SubscribeEvent
     public static void registerColoredItems(RegisterColorHandlersEvent.Item event) {
-        event.register((pStack, pTintIndex) -> {
-            BlockState state = ((BlockItem)pStack.getItem()).getBlock().defaultBlockState();
-            return event.getBlockColors().getColor(state, null, null, pTintIndex);
-        }, ModBlocks.COLORED_LEAVES.get());
+        event.register((stack, tintIndex) -> {
+            BlockState state = ((BlockItem)stack.getItem()).getBlock().defaultBlockState();
+            return event.getBlockColors().getColor(state, null, null, tintIndex); },
+            ModBlocks.COLORED_LEAVES.get());
     }
 
+    // CUSTOM EVENT - Register custom Key Input
     @SubscribeEvent
     public static void registerKeyInput(RegisterKeyMappingsEvent event) {
-        // Register custom Key Input
         event.register(KeyBinding.GLOWING_BLOCKS_KEY); // Glowing Blocks custom enchantment
         event.register(KeyBinding.GLOWING_MOBS_KEY); // Glowing Mobs custom enchantment
         event.register(KeyBinding.MCCOURSE_BOTTLE_STORED_TEN_LEVELS_KEY); // Mccourse Bottle stored 10 levels
@@ -69,9 +68,9 @@ public class ModEventClientBusEvents {
         event.register(KeyBinding.UNLOCK_KEY); // Unlock custom enchantment
     }
 
+    // CUSTOM EVENT - Register custom image tooltip
     @SubscribeEvent
     public static void registerTooltip(RegisterClientTooltipComponentFactoriesEvent event) {
-        // Register custom image tooltip
-        event.register(ImageTooltipComponent.class, Function.identity());
+        event.register(ImageTooltipComponent.class, Function.identity()); // Unlock custom enchantment
     }
 }
